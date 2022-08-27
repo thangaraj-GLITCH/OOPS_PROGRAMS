669afc9d350f23a50608663b20d418fca52ddc2c
thangaraj-GLITCH <112239922+thangaraj-GLITCH@users.noreply.github.com>
2022-08-27 14:17:08 +0530

    Add files via upload

---
+++ default constructorr.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+using namespace std;  
+class Employee  
+ {  
+   public:  
+   Employee()    
+   {    
+   cout<<"Default Constructor Invoked"<<endl;    
+   }    
+};  
+int main(void)   
+{  
+    Employee e1;   
+    Employee e2;   
+    return 0;  
+}  
